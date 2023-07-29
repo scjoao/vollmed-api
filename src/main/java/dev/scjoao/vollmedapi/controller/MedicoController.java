43756712cbf066cdc670c93893d02e1bb6589d75
João Carlos <joao.carlos_25@hotmail.com>
2023-07-29 11:37:44 -0300

    Listagem de Medicos

--- src/main/java/dev/scjoao/vollmedapi/controller/MedicoController.java
+++ src/main/java/dev/scjoao/vollmedapi/controller/MedicoController.java
@@ -1,12 +1,15 @@
 package dev.scjoao.vollmedapi.controller;
 
 import dev.scjoao.vollmedapi.medico.DadosCadastroMedico;
+import dev.scjoao.vollmedapi.medico.DadosListagemMedico;
 import dev.scjoao.vollmedapi.medico.Medico;
 import dev.scjoao.vollmedapi.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,6 +22,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico){
         repository.save(new Medico(dadosCadastroMedico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
 
