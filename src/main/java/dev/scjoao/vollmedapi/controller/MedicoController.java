16865c8da70c7ee6d174e4dadf7259a9f165e91f
João Carlos <joao.carlos_25@hotmail.com>
2023-07-29 12:02:19 -0300

    Paginação Medicos

--- src/main/java/dev/scjoao/vollmedapi/controller/MedicoController.java
+++ src/main/java/dev/scjoao/vollmedapi/controller/MedicoController.java
@@ -7,6 +7,8 @@ import dev.scjoao.vollmedapi.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +27,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
 
